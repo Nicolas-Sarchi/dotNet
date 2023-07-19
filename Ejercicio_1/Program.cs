ce5210ecb4ecc0ea5e67276cab582b724254c61b
Nicolas sarchi <nicolassarchipico@gmail.com>
2023-07-19 11:45:04 -0500

    primeros ejercicios c#

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un número: ");
+string num = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Ingrese un número: ");
+string num2 = Console.ReadLine() ?? string.Empty;
+
+double suma = double.Parse(num) + double.Parse(num2);
+double resta = double.Parse(num) - double.Parse(num2);
+double multiplicacion = double.Parse(num) * double.Parse(num2);
+double division = double.Parse(num) / double.Parse(num2);
+
+Console.WriteLine($"Suma: {suma}" + " " + $"Resta: {resta}" + " " + $"Multiplicacion: {multiplicacion}" + " " + $"Division: {division}");
+
+
---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un Numero: ");
+string num = Console.ReadLine() ?? string.Empty;	
+
+if(long.Parse(num) == 0)
+{
+    Console.WriteLine("El numero es 0");
+}
+else if (long.Parse(num) % 2 == 0)
+{
+    Console.WriteLine("Es par");
+} 
+else
+{
+    Console.WriteLine("Es impar");
+}
